b86daa83f38af9d474e808adbb081c9dc6e628fa
Artem Artemyev <artem.artemyev@springernature.com>
2020-03-20 11:57:47 +0100

    Stupid gravity

--- sketch.js
+++ sketch.js
@@ -27,12 +27,36 @@ function Particle(pos, vel) {
 function setup() {
     createCanvas(400, 400);
 
-    particles.push(new Particle(createVector(40.0, 40.0), createVector(0.5, 0.1)));
+    particles.push(new Particle(createVector(40.0, 40.0), createVector(0.0, 0.0)));
+    particles.push(new Particle(createVector(120.0, 120.0), createVector(0.0, 0.0)));
 }
 
 function draw() {
     clear();
     background(51);
+
+    for (let i = 0; i < particles.length; i++) {
+        let p = particles[i];
+        let resultForce = createVector(0.0, 0.0);
+
+        // Calculate resulting force
+        for (let j = 0; j < particles.length; j++) {
+            let q = particles[j];
+
+            let distanse = p5.Vector.dist(p.pos, q.pos);
+
+            let direction = p5.Vector.sub(q.pos, p.pos).normalize();
+            let force = direction.mult(0.01).div(distanse * distanse);
+            resultForce.add(force);
+
+        }
+
+        // Apply force
+        let dv = p5.Vector.mult(resultForce, deltaTime);
+
+        p.vel.add(dv);
+    }
+
 
     for (let i = 0; i < particles.length; i++) {
         particles[i].update();
