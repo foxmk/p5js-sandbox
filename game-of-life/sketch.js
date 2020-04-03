fc8384bdd7c1d9ba52dd807dd797f92711a9d07e
Artem Artemyev <artem.artemyev@springernature.com>
2020-04-03 12:46:00 +0200

    Refactor + fix indexing

--- game-of-life/sketch.js
+++ game-of-life/sketch.js
@@ -11,39 +11,72 @@ let next = [];
 function setup() {
     let canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('sketch');
-
-    for (let i = 0; i <= colCount; i++) {
-        prev.push([]);
-        next.push([]);
-        for (let j = 0; j <= rowCount; j++) {
-            prev[i].push(random() <= 0.1);
-            next[i].push(false);
-        }
-    }
-
+    initGrids();
     frameRate(10);
 }
 
 function draw() {
     clear();
+    updateCells();
+    drawCells();
+    swapGrids();
+}
 
-    for (let i = 0; i <= colCount; i++) {
-        for (let j = 0; j <= rowCount; j++) {
+function initGrids() {
+    for (let row = 0; row < rowCount; row++) {
+        prev.push([]);
+        next.push([]);
+        for (let col = 0; col < colCount; col++) {
+            prev[row].push(random() <= 0.1);
+            next[row].push(false);
+        }
+    }
+}
+
+function swapGrids() {
+    let temp = prev;
+    prev = next;
+    next = temp;
+}
+
+function drawCells() {
+    for (let row = 0; row < rowCount; row++) {
+        for (let col = 0; col < colCount; col++) {
+            let x = col * cellSize;
+            let y = row * cellSize;
+
+            let c;
+            if (next[col][row] === true) {
+                c = color(0);
+            } else {
+                c = color(255);
+            }
+            fill(c);
+            rect(x, y, cellSize, cellSize);
+        }
+    }
+}
+
+function updateCells() {
+    for (let i = 0; i < rowCount; i++) {
+        for (let j = 0; j < colCount; j++) {
             let liveNeighbourCnt = 0;
 
-            let prevCol = i === 0 ? colCount : i - 1;
-            let nextCol = i === colCount ? 0 : i + 1;
-            let prevRow = j === 0 ? rowCount : j - 1;
-            let nextRow = j === rowCount ? 0 : j + 1;
+            let prevCol = j === 0 ? colCount - 1 : j - 1;
+            let nextCol = j === colCount - 1 ? 0 : j + 1;
+            let prevRow = i === 0 ? rowCount - 1 : i - 1;
+            let nextRow = i === rowCount - 1 ? 0 : i + 1;
 
-            if (prev[prevCol][prevRow]) liveNeighbourCnt++;
-            if (prev[prevCol][j]) liveNeighbourCnt++;
-            if (prev[prevCol][nextRow]) liveNeighbourCnt++;
-            if (prev[i][prevRow]) liveNeighbourCnt++;
-            if (prev[i][nextRow]) liveNeighbourCnt++;
-            if (prev[nextCol][prevRow]) liveNeighbourCnt++;
-            if (prev[nextCol][j]) liveNeighbourCnt++;
-            if (prev[nextCol][nextRow]) liveNeighbourCnt++;
+            if (prev[prevRow][prevCol]) liveNeighbourCnt++;
+            if (prev[prevRow][j]) liveNeighbourCnt++;
+            if (prev[prevRow][nextCol]) liveNeighbourCnt++;
+
+            if (prev[i][prevCol]) liveNeighbourCnt++;
+            if (prev[i][nextCol]) liveNeighbourCnt++;
+
+            if (prev[nextRow][prevCol]) liveNeighbourCnt++;
+            if (prev[nextRow][j]) liveNeighbourCnt++;
+            if (prev[nextRow][nextCol]) liveNeighbourCnt++;
 
 
             if (prev[i][j] && (liveNeighbourCnt === 3 || liveNeighbourCnt === 2)) {
@@ -58,25 +91,4 @@ function draw() {
             }
         }
     }
-
-
-    for (let col = 0; col <= colCount; col++) {
-        for (let row = 0; row <= rowCount; row++) {
-            let x = col * cellSize;
-            let y = row * cellSize;
-
-            let c;
-            if (next[col][row] === true) {
-                c = color(0);
-            } else {
-                c = color(255);
-            }
-            fill(c);
-            rect(x, y, cellSize, cellSize);
-        }
-    }
-
-    let temp = prev;
-    prev = next;
-    next = temp;
 }
