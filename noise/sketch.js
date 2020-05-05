799daef45a2cc456ca7608e39b892442adb1e624
Artem Artemyev <artem.artemyev@springernature.com>
2020-05-05 22:17:52 +0200

    Extract Poisson disc algo

--- noise/sketch.js
+++ noise/sketch.js
@@ -4,63 +4,23 @@ const MAX_SAMPLE_RADIUS = 30.0
 const WIDTH = 500
 const HEIGHT = 500
 
-let activePoints = []
 let samples = []
-let seeded = false
 
 function setup () {
   let canvas = createCanvas(WIDTH, HEIGHT)
   canvas.parent('sketch')
 
-  while (samplePoint() !== null) {}
-}
+  let sampler = new PoissonSampler(MIN_SAMPLE_RADIUS, MAX_SAMPLE_RADIUS, WIDTH, HEIGHT, MAX_SAMPLES)
 
-function samplePoint () {
-  function generateCandidate (point) {
-    let a = p5.Vector.random2D()
-    a.setMag(random(MIN_SAMPLE_RADIUS / 2.0, MAX_SAMPLE_RADIUS / 2.0))
-    return p5.Vector.add(a, point)
-  }
+  let done = false
 
-  function isFarEnough (candidate) {
-    for (let p of samples) {
-      if (p5.Vector.dist(p, candidate) < MIN_SAMPLE_RADIUS) {
-        return false
-      }
-    }
-    return true
-  }
-
-  function isWithinBorders (candidate) {
-    return candidate.x > 0 && candidate.x <= WIDTH && candidate.y > 0 && candidate.y <= HEIGHT
-  }
-
-  if (activePoints.length === 0) {
-    if (!seeded) {
-      let p = createVector(random(0, WIDTH), random(0, HEIGHT))
-      activePoints.push(p)
-      samples.push(p)
-      seeded = true
-      return p
+  while (!done) {
+    let p = sampler.sample()
+    if (p === null) {
+      done = true
     } else {
-      return null
+      samples.push(p)
     }
-  }
-
-  while (activePoints.length > 0) {
-    let point = random(activePoints)
-
-    for (let j = 0; j < MAX_SAMPLES; j++) {
-      let candidate = generateCandidate(point)
-
-      if (isFarEnough(candidate) && isWithinBorders(candidate)) {
-        activePoints.push(candidate)
-        samples.push(candidate)
-        return candidate
-      }
-    }
-
-    activePoints.splice(activePoints.indexOf(point), 1)
   }
 }
 
@@ -68,7 +28,6 @@ let xoff = 0
 let yoff = 0
 let hxoff = 0
 let hyoff = 0
-
 
 function draw () {
   clear()
